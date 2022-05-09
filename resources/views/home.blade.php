5a32d68521367d05f97907f112131102a07e7856
Randy <evilryok@gmail.com>
2022-05-09 11:11:37 +0800

    change type

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -15,7 +15,7 @@
             <div class="pt-2 relative mx-auto text-gray-600 flex flex-wrap justify-between md:flex-row md:w-1/2 w-full" id="keyword_search">
                 <form method="POST" action="/search" class="flex flex-initial justify-between md:flex-row border-2 bg-white items-center rounded-md w-full group" id="search-keyword">
                     @csrf
-                    <input id="typeahead" type="text" name="search" placeholder="Ex: Facebook Login..." required="required" class="typeahead w-full flex-1 h-12 md:h-10 px-4 rounded-none text-gray-700 placeholder-gray-400 bg-transparent border-none appearance-none lg:h-12 dark:text-gray-200 group-focus:outline-none group-focus:placeholder-transparent group-focus:ring-0"> 
+                    <input id="typeahead" type="search" name="search" placeholder="Ex: Facebook Login..." required="required" class="typeahead w-full flex-1 h-12 md:h-10 px-4 rounded-none text-gray-700 placeholder-gray-400 bg-transparent border-none appearance-none lg:h-12 dark:text-gray-200 group-focus:outline-none group-focus:placeholder-transparent group-focus:ring-0"> 
                     <button id="submitBtn" type="button" class="flex items-center justify-center text-white transition-colors duration-300 transform rounded-tr-md rounded-br-md h-12 w-16 lg:w-16 lg:h-12 lg:p-0 bg-darkblue hover:bg-darkblue/70 focus:outline-none focus:bg-darkblue/70">
                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" class="w-6 h-6">
                             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path>
