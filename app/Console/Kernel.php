e89fd953c1c6540e3a1e177d9a523f9130b9c3c3
Randy <evilryok@gmail.com>
2022-05-12 15:32:14 +0800

    Update Kernel.php

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -4,9 +4,19 @@ namespace App\Console;
 
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
+use App\Console\Commands\SerpMasterKeyword;
 
 class Kernel extends ConsoleKernel
 {
+   /**
+     * The Artisan commands provided by your application.
+     *
+     * @var array
+     */
+    protected $commands = [
+        SerpMasterKeyword::class,
+    ];
+
     /**
      * Define the application's command schedule.
      *
@@ -15,7 +25,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        // $schedule->command('inspire')->hourly();
+        $schedule->command('serpKeywords:master 1000')->daily();
     }
 
     /**
