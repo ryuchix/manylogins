47eb41fb4b1042da2a689294092aa6181e485ca6
Randy <evilryok@gmail.com>
2022-05-16 14:23:12 +0800

    set footer logo

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -3,7 +3,7 @@
         <div class="relative flex items-center justify-between h-16 md:h-20">
             <div class="flex-1 flex items-center sm:items-stretch sm:justify-start">
                 <div class="flex-shrink-0 flex items-center">
-                <a href="{{ route('home') }}"><img src="{{ asset('image/logo.png') }}" alt="Logo" class="md:h-10 h-8"></a>
+                <a href="{{ route('home') }}"><img src="{{ asset('images/settings/'.$setting->header_logo) }}" alt="Logo" class="md:h-10 h-8"></a>
             </div>
         </div>
     </div>
