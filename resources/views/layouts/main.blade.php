ba5e6fea87e35fe18f4a4a0b2eee74f57b0a8d84
Randy <evilryok@gmail.com>
2022-05-16 13:56:45 +0800

    add open graph

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -9,6 +9,14 @@
         <meta name="keyword" content="@yield('keyword', $setting->site_keywords)">
         <meta name="description" content="@yield('description', $setting->site_description)">
 
+        <meta property="og:title" content="@yield('title', $setting->site_title)" />
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="{{ \URL::current() }}" />
+        <meta property="og:image" content="{{ asset('images/settings/'.$setting->footer_logo) }}" />
+        <meta property="og:image:alt" content="{{ $setting->site_title }}" />
+        <meta property="og:description" content="@yield('description', $setting->site_description)" />
+        <meta property="og:locale" content="en_GB" />
+
         <link rel="stylesheet" href="{{ asset('css/app.css?t='.time()) }}">
 
         <link rel="apple-touch-icon" sizes="57x57" href="{{ asset('favicons') }}/apple-icon-57x57.png">
