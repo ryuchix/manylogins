2b72891f305c09c536239dabe928c47b3a1aa7bb
Randy <evilryok@gmail.com>
2022-05-19 16:57:47 +0800

    add links

--- app/Http/Controllers/SitemapController.php
+++ app/Http/Controllers/SitemapController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Carbon\Carbon;
 
 use App\Models\KeywordSearch;
+use App\Models\Post;
 
 use Illuminate\Http\Request;
 
@@ -26,6 +27,17 @@ class SitemapController extends Controller
                     $sitemapName = 'keywords_sitemap'.$chunk.'.xml';
                     $sitemap = Sitemap::create();
 
+                    $sitemap->add(Url::create('/')
+                            ->setLastModificationDate(Carbon::now()));
+
+                    $sitemap->add(Url::create('blogs')
+                            ->setLastModificationDate(Carbon::now()));
+
+                    foreach ($Post as $post) {
+                        $sitemap->add(Url::create($post->slug)
+                                ->setLastModificationDate($post->updated_at));
+                    }
+
                     foreach ($products as $product) {
                         $sitemap->add(Url::create($product->slug)
                                 ->setLastModificationDate($product->updated_at));
@@ -39,11 +51,11 @@ class SitemapController extends Controller
 
             return redirect('sitemap.xml');
         } catch (\Exception $e) {
-            logger($e);
+            logger($e->getMessage());
         }  catch (\Throwable $e) {
-            logger($e);
+            logger($e->getMessage());
         } catch (\InvalidArgumentException $th) {
-            logger(json_encode($th));
+            logger(($th->getMessage()));
         }
     }
 }
