6fa98e4ced1f2ef3f7ea655e549e578ecb0e3b62
Randy <evilryok@gmail.com>
2022-05-23 16:50:36 +0800

    confirm delete

--- resources/views/admin/blogs/index.blade.php
+++ resources/views/admin/blogs/index.blade.php
@@ -68,7 +68,7 @@
                                     <form action="{{ route('posts.destroy', $post->id) }}" method="POST" class="flex items-center">
                                         @csrf
                                         @method('DELETE')
-                                        <button type="submit" class="w-4 mr-2 transform hover:text-purple-500 hover:scale-110 cursor-pointer">
+                                        <button type="button" class="delete-blog-btn w-4 mr-2 transform hover:text-purple-500 hover:scale-110 cursor-pointer">
                                             <svg class="w-4 h-4" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                                                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16" />
                                             </svg>
@@ -84,4 +84,23 @@
         </div>
     </div>
 </div>
+@endsection
+
+@section('script')
+
+<script>
+    $('.delete-blog-btn').on('click', function(e) {
+        var self = $(this).parent('form');
+        Swal.fire({
+            title: 'Are you sure?',
+            text: 'This process is permanent!',
+            showCancelButton: true,
+            confirmButtonText: 'Delete',
+        }).then((result) => {
+            if (result.isConfirmed) {
+                self.trigger('submit');
+            }
+        })
+    });
+</script>
 @endsection
