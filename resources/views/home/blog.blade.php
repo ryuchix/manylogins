fc821ce6a553cba0666fc302062c12ddd287ae8c
Randy <evilryok@gmail.com>
2022-05-26 12:57:14 +0800

    broken image

--- resources/views/home/blog.blade.php
+++ resources/views/home/blog.blade.php
@@ -69,7 +69,7 @@
             <div class="grid justify-center md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-5 lg:gap-7 mt-3 mb-10">
                 @foreach($related as $post)
                 <div class="article h-full rounded-md shadow-cla-pink bg-gradient-to-r from-fuchsia-50 to-pink-50 overflow-hidden">
-                    <img class="lg:h-42 md:h-28 w-full object-cover object-centertransition-all duration-400" src="{{ asset('images/posts/'.$post->cover) }}" alt="{{ $post->title }}">
+                    <img class="lg:h-42 md:h-28 w-full object-cover object-centertransition-all duration-400" src="{{ asset('storage/'.$post->cover) }}" alt="{{ $post->title }}">
                     <div class="px-4 py-4">
                         <h1 class="title-font text-base font-medium mb-1 line-clamp-none md:line-clamp-2"><a href="{{ route('show.blog', ['blog' => $post->slug]) }}" class="block text-xl text-link hover:text-opacity-80">{{ $post->title }}</a></h1>
                         <div class="leading-relaxed mb-3 text-gray-500 text-sm">{!! substr_replace(strip_tags($post->content), strlen($post->content) <= 100 ? "" : "...", 100); !!}</div>
