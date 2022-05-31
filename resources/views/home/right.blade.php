6fa12c96800ec931b6746e9d2c8c5737a0ede59a
Randy <evilryok@gmail.com>
2022-05-31 17:51:32 +0800

    Update right.blade.php

--- resources/views/home/right.blade.php
+++ resources/views/home/right.blade.php
@@ -50,7 +50,7 @@
             <div class="p-2 w-full flex-col space-y-4 group">
                 <div class="article  h-full rounded-md shadow-cla-pink bg-gradient-to-r from-fuchsia-50 to-pink-50 overflow-hidden">
                     <img class="lg:h-48 md:h-36 w-full object-cover object-centertransition-all duration-400" src="{{ asset($post->cover) }}" alt="{{ $post->title }}">
-                    <div class="flex flex-wrap justify-start items-center mt-2 pt-2 px-4 lg:space-y-1 xl:space-y-0">
+                    <div class="flex flex-wrap justify-start items-center mt-2 pt-2 px-4">
                         @foreach ($post->categories as $cat)
                         <div class="text-xs mr-2 mt-1 py-1.5 px-4 text-gray-200 bg-[#718096] rounded-2xl hover:text-white hover:bg-link cursor-pointer">
                             {{ $cat->name }}
