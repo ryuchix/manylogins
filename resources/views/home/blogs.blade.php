1400e149ecd877893de07e785b96df68cfbcd96c
Randy <evilryok@gmail.com>
2022-06-03 13:44:19 +0800

    blogs

--- resources/views/home/blogs.blade.php
+++ resources/views/home/blogs.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.main')
 
-@section('title', 'Blog Lists' . ' - ' . $setting->site_title)
-@section('keyword', 'Blog list, Many Login blogs, Manylogin posts, Read blog at Many Login')
+@section('title', 'Guides' . ' - ' . $setting->site_title)
+@section('keyword', 'Guides, Many Login Guides, Manylogin posts, Read blog at Many Login')
 @section('description', substr_replace('Read articles and blogs provided by Many Logins. Get motivated and learn tips, hacks from the expert', strlen('Read articles and blogs provided by Many Logins. Get motivated and learn tips, hacks from the expert') <= 200 ? "" : "...", 200) )
 
 @section('style')
@@ -15,7 +15,7 @@
     </h1>
     <div class="grid justify-center md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-3 2xl:grid-cols-4 gap-5 lg:gap-7 mt-3 mb-10">
         @foreach($blogs as $blog)
-        <div class="article h-full rounded-md shadow-cla-pink bg-gradient-to-r from-fuchsia-50 to-pink-50 overflow-hidden">
+        <div class="article h-full rounded-md shadow-cla-pink bg-white overflow-hidden">
             <img class="lg:h-42 md:h-28 w-full object-cover object-centertransition-all duration-400" src="{{ asset($blog->cover) }}" alt="{{ $blog->title }}">
             <div class="px-4 py-4">
                 <h1 class="title-font text-base font-medium mb-1 line-clamp-none md:line-clamp-2"><a href="{{ route('show.blog', ['blog' => $blog->slug]) }}" class="block text-xl text-link hover:text-opacity-80">{{ $blog->title }}</a></h1>
