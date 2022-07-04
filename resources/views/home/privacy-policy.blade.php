8645b3da6e1f771b3103a4bff4a07a84ea8574d5
Randy <evilryok@gmail.com>
2022-07-04 17:26:27 +0800

    Update privacy-policy.blade.php

--- resources/views/home/privacy-policy.blade.php
+++ resources/views/home/privacy-policy.blade.php
@@ -4,6 +4,10 @@
 @section('keyword', strip_tags(substr_replace($setting->privacy, strlen($setting->privacy) <= 500 ? "" : "...", 500)))
 @section('description', strip_tags(substr_replace($setting->privacy, strlen($setting->privacy) <= 500 ? "" : "...", 500)))
 
+@section('style')
+<link rel="stylesheet" href="{{ asset('css/blog.css?t='.time()) }}">
+@endsection
+
 @section('content')
     <div class="flex flex-col pb-10 pt-6 md:px-5 px-1 w-full blog-content ">
         <h1 class="title capitalize font-semibold text-gray-700 text-5xl mb-10">Privacy Policy</h1>
